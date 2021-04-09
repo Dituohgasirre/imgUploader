4bda0141639e522012e78540c60d210b332557f5
Dituohgasirre <linfeiji4729289@126.com>
2021-04-09 14:52:03 +0800

    add modify 500

--- helpers/stats.js
+++ helpers/stats.js
@@ -2,21 +2,26 @@ const mongoose = require('mongoose')
 
 const Image = mongoose.model('Image')
 
-const stats = () =>
-    Image.aggregate([
-        {$group: {
-            _id: 'total',
-            images: {$sum: 1},
-            views: {$sum: '$views'},
-            likes: {$sum: '$likes'}
+module.exports = () =>
+    Image.aggregate().group({
+        _id:'total',
+        images: {$sum: 1},
+        views: {$sum: '$views'},
+        likes: {$sum: '$likes'}
+    })
+    .then(doc =>{
+        if (doc.length == 0) {
+            return{
+                images: 0,
+                views: 0,
+                likes: 0,
+                comments: 0
             }
         }
-    ])
-    .then(arr => ({
-        images: arr[0].images,
-        views: arr[0].views,
-        likes: arr[0].likes,
-        comments: 0
-    }))
-
-module.exports = stats
+        return {
+            images: doc[0].images,
+            views: doc[0].views,
+            likes: doc[0].likes,
+            comments: 0
+        }
+    })
